c2a230a51028c3ed4be22e9deecbcb9a08251ec3
Josip Medved <jmedved@jmedved.com>
2016-08-10 22:21:43 -0700

    Auto-type processing functions.

--- Source/Bimil/Properties/AssemblyInfo.cs
+++ Source/Bimil/Properties/AssemblyInfo.cs
@@ -11,3 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
+
+[assembly: InternalsVisibleTo("Bimil.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100dd86a004339478cde1e8293deea855a5282b4bc1fa5783f852719ae6c8ab0a3db047b94e6fb2619d976891dae70b86b67471398f86fddc7c6bd93a169a2dee4c17a57a5e34114aa4de938c369149645033f926e527b4e7206bc080ec09c27c2d8ed83d270f17773ca82fa0f212be721cf6a1b2d9c568c9134717ed6ad38042ac")]
