c7e2f57155d7786c8e1008fdf35445b67b485c51
Josip Medved <jmedved@jmedved.com>
2017-06-22 22:44:24 -0700

    Changed creation of Windows Store package.

--- Setup/Publish.cmd
+++ Setup/Publish.cmd
@@ -5,14 +5,12 @@ SET                   NAME="Bimil"
 SET                  FILES="..\Binaries\Bimil.exe" "..\LICENSE.md" "..\README.md" "..\WORDS.md"
 SET        SOURCE_SOLUTION="..\Source\Bimil.sln"
 SET       SOURCE_INNOSETUP=".\Bimil.iss"
-SET             SOURCE_WIX=".\Bimil.wxs"
 
 SET              TOOLS_GIT="%PROGRAMFILES%\Git\mingw64\bin\git.exe"
 SET     TOOLS_VISUALSTUDIO="%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe"
 SET         TOOLS_SIGNTOOL="%PROGRAMFILES(X86)%\Microsoft SDKs\ClickOnce\SignTool\signtool.exe" "%PROGRAMFILES(X86)%\Windows Kits\10\App Certification Kit\signtool.exe" "%PROGRAMFILES(X86)%\Windows Kits\10\bin\x86\signtool.exe"
 SET        TOOLS_INNOSETUP="%PROGRAMFILES(x86)%\Inno Setup 5\iscc.exe"
 SET           TOOLS_WINRAR="%PROGRAMFILES%\WinRAR\WinRAR.exe"
-SET    TOOLS_WIX_DIRECTORY="%PROGRAMFILES(x86)%\WiX Toolset v3.11\bin"
 
 SET CERTIFICATE_THUMBPRINT="df26e797ffaee47a40c1fab756e995d3763da968"
 SET      SIGN_TIMESTAMPURL="http://timestamp.comodoca.com/rfc3161"
@@ -56,16 +54,6 @@ FOR %%I IN (%TOOLS_WINRAR%) DO (
 )
 IF [%TOOL_WINRAR%]==[] SET WARNING=1
 ECHO WinRAR ......: %TOOL_WINRAR%
-
-FOR %%I IN (%TOOLS_WIX_DIRECTORY%) DO (
-    IF EXIST %%I\candle.exe IF EXIST %%I\light.exe IF NOT DEFINED TOOL_WIX_CANDLE (
-        SET TOOL_WIX_CANDLE="%%~I\candle.exe"
-        SET TOOL_WIX_LIGHT="%%~I\light.exe"
-    )
-)
-IF [%TOOL_WIX_CANDLE%]==[] SET WARNING=1
-ECHO Wix (candle) : %TOOL_WIX_CANDLE%
-ECHO Wix (light) .: %TOOL_WIX_LIGHT%
 
 IF NOT [%CERTIFICATE_THUMBPRINT%]==[] (
     CERTUTIL -silent -verifystore -user My %CERTIFICATE_THUMBPRINT% > NUL
@@ -186,43 +174,6 @@ IF NOT [%TOOL_WINRAR%]==[] (
 )
 
 
-IF NOT [%TOOL_WIX_CANDLE%]==[] (
-    ECHO --- BUILD MSI INSTALLER
-    ECHO:
-
-    COPY NUL ..\Binaries\Bimil.cfg > NUL
-
-    %TOOL_WIX_CANDLE% -out Temp\Setup.wixobj -nologo -pedantic %SOURCE_WIX% > NUL
-    IF NOT ERRORLEVEL 0 GOTO Error
-
-    %TOOL_WIX_LIGHT% -out Temp\!SETUPEXE:.exe=.msi! -b ..\Binaries -nologo -pedantic -sice:ICE91 -sacl -spdb Temp\Setup.wixobj
-    IF NOT ERRORLEVEL 0 GOTO Error
-
-    DEL Temp\Setup.wixobj
-    DEL ..\Binaries\Bimil.cfg
-    
-    ECHO MSI created.
-    
-    ECHO:
-    ECHO:
-
-    IF NOT [%TOOL_SIGNTOOL%]==[] IF NOT [%CERTIFICATE_THUMBPRINT%]==[] (
-        ECHO --- SIGN MSI INSTALLER
-        ECHO:
-        
-        IF [%SIGN_TIMESTAMPURL%]==[] (
-            %TOOL_SIGNTOOL% sign /s "My" /sha1 %CERTIFICATE_THUMBPRINT% /v /d "%NAME% Installer" ".\Temp\!SETUPEXE:.exe=.msi!"
-        ) ELSE (
-            %TOOL_SIGNTOOL% sign /s "My" /sha1 %CERTIFICATE_THUMBPRINT% /tr %SIGN_TIMESTAMPURL% /v /d "%NAME% Installer" ".\Temp\!SETUPEXE:.exe=.msi!"
-        )
-        IF NOT ERRORLEVEL 0 GOTO Error
-
-        ECHO:
-        ECHO:
-    )
-)
-
-
 ECHO --- RELEASE
 ECHO:
 
@@ -255,7 +206,6 @@ IF NOT [%WARNING%]==[] (
     IF [%TOOL_SIGNTOOL%]==[] ECHO SignTool executable not found.
     IF [%TOOL_INNOSETUP%]==[] ECHO InnoSetup executable not found.
     IF [%TOOL_WINRAR%]==[] ECHO WinRAR executable not found.
-    IF [%TOOL_WIX_CANDLE%]==[] ECHO Wix executables not found.
 
     IF [%CERTIFICATE_THUMBPRINT%]==[] ECHO Executables not signed.
     PAUSE
