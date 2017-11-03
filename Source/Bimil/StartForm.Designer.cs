d50fe779d0c14c5e0aa62d4b5ccbae6c9e65a39b
Josip Medved <jmedved@jmedved.com>
2017-11-02 21:53:25 -0700

    Changed backcolor for start form.

--- Source/Bimil/StartForm.Designer.cs
+++ Source/Bimil/StartForm.Designer.cs
@@ -176,7 +176,6 @@ namespace Bimil {
             this.AcceptButton = this.btnOpen;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
             this.CancelButton = this.btnClose;
             this.ClientSize = new System.Drawing.Size(482, 353);
             this.Controls.Add(this.btnOpenReadOnly);
