88e3dc2c86a335f087b24032f62dae29933fddf1
Josip Medved <jmedved@jmedved.com>
2018-01-12 23:28:25 -0800

    Fixed version - accidentally skipped by 1.00.

--- Source/Bimil/Properties/SolutionInfo.cs
+++ Source/Bimil/Properties/SolutionInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Josip Medved")]
 [assembly: AssemblyCopyright("Copyright (c) 2010 Josip Medved <jmedved@jmedved.com>")]
 
-[assembly: AssemblyVersion("3.40.*")]
-[assembly: AssemblyInformationalVersion("3.40")]
+[assembly: AssemblyVersion("2.40.*")]
+[assembly: AssemblyInformationalVersion("2.40")]
