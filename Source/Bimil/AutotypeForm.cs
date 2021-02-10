317ec444051d4dae9267064a79ad63433d38f86a
Josip Medved <jmedved@jmedved.com>
2021-02-09 20:47:07 -0800

    Handling escaped } in Linux auto-type

--- Source/Bimil/AutotypeForm.cs
+++ Source/Bimil/AutotypeForm.cs
@@ -185,21 +185,19 @@ namespace Bimil {
                                 }
                             }
                         } else {
-                            if (ch == '}') {
-                                if (sbSpecialKey.Length > 0) {
-                                    if (sbArgKeys.Length > 0) { sbArgKeys.Append(" "); }
-                                    var specialKey = sbSpecialKey.ToString();
-                                    if (specialKey.Length == 1) { // to handle +^%~(){}[]
-                                        var charBytes = Utf8Encoding.GetBytes(specialKey.ToString(CultureInfo.InvariantCulture));
-                                        sbArgKeys.Append(charBytes.Length < 2 ? "0x00" : "0x");
-                                        foreach (var charByte in charBytes) {  // get hex values for each char
-                                            sbArgKeys.Append(charByte.ToString("X2", CultureInfo.InvariantCulture));
-                                        }
-                                    } else {
-                                        switch (specialKey) {
-                                            case "Enter": sbArgKeys.Append("Return"); break;
-                                            default: sbArgKeys.Append(specialKey); break;
-                                        }
+                            if ((ch == '}') && (sbSpecialKey.Length > 0)) {  // to handle escaped }
+                                if (sbArgKeys.Length > 0) { sbArgKeys.Append(" "); }
+                                var specialKey = sbSpecialKey.ToString();
+                                if (specialKey.Length == 1) { // to handle +^%~(){}[]
+                                    var charBytes = Utf8Encoding.GetBytes(specialKey.ToString(CultureInfo.InvariantCulture));
+                                    sbArgKeys.Append(charBytes.Length < 2 ? "0x00" : "0x");
+                                    foreach (var charByte in charBytes) {  // get hex values for each char
+                                        sbArgKeys.Append(charByte.ToString("X2", CultureInfo.InvariantCulture));
+                                    }
+                                } else {
+                                    switch (specialKey) {
+                                        case "Enter": sbArgKeys.Append("Return"); break;
+                                        default: sbArgKeys.Append(specialKey); break;
                                     }
                                 }
                                 sbSpecialKey.Length = 0;
