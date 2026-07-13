f72f1c00e0060c99c302dc0cc24fcb3189739201
Josip Medved <jmedved@jmedved.com>
2026-07-12 21:59:49 -0700

    Added wildcard search

--- src/Bimil.Common/State.cs
+++ src/Bimil.Common/State.cs
@@ -3,6 +3,7 @@ namespace Bimil;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using Medo.Security.Cryptography.PasswordSafe;
 
 /// <summary>
@@ -115,29 +116,44 @@ public sealed class State {
     public IReadOnlyList<Entry> GetEntries(string filter, string? group, bool includeHidden = false) {
         if (Document == null) { return Array.Empty<Entry>(); }
 
-        var filterWords = filter.Split(Array.Empty<char>(), StringSplitOptions.RemoveEmptyEntries);
-
         var list = new List<Entry>();
-        foreach (var entry in Document.Entries) {
-            if ((group == null) || string.Equals(entry.Group, group, StringComparison.CurrentCultureIgnoreCase)) {
-                var isHidden = entry.Title.StartsWith('.');
-                if (isHidden && !includeHidden) { continue; }
 
+        if (filter.Contains('*', StringComparison.Ordinal)) {  // wildcard search
+            var regexPattern = Regex.Escape(filter).Replace(@"\*", ".*");
+            var regex = new Regex(regexPattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
-                if (filterWords.Length > 0) {
-                    var allMatched = true;
-                    foreach (var word in filterWords) {
-                        if ((entry.Title.IndexOf(word, StringComparison.CurrentCultureIgnoreCase) < 0)
-                        && (entry.Group.ToString().IndexOf(word, StringComparison.CurrentCultureIgnoreCase) < 0)) {
-                            allMatched = false;
-                            break;
-                        }
-                    }
-                    if (!allMatched) { continue; }
-                } else if ((group == null) && (entry.Group != "")) {
-                    continue;  // if group is not set, skip entries with group
+            foreach (var entry in Document.Entries) {
+                if ((group == null) || string.Equals(entry.Group, group, StringComparison.CurrentCultureIgnoreCase)) {
+                    var isHidden = entry.Title.StartsWith('.');
+                    if (isHidden && !includeHidden) { continue; }
+
+                    if (!regex.IsMatch(entry.Title)) { continue; }
+                    list.Add(entry);
                 }
-                list.Add(entry);
+            }
+        } else {  // word search
+            var filterWords = filter.Split(Array.Empty<char>(), StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var entry in Document.Entries) {
+                if ((group == null) || string.Equals(entry.Group, group, StringComparison.CurrentCultureIgnoreCase)) {
+                    var isHidden = entry.Title.StartsWith('.');
+                    if (isHidden && !includeHidden) { continue; }
+
+                    if (filterWords.Length > 0) {
+                        var allMatched = true;
+                        foreach (var word in filterWords) {
+                            if ((entry.Title.IndexOf(word, StringComparison.CurrentCultureIgnoreCase) < 0)
+                            && (entry.Group.ToString().IndexOf(word, StringComparison.CurrentCultureIgnoreCase) < 0)) {
+                                allMatched = false;
+                                break;
+                            }
+                        }
+                        if (!allMatched) { continue; }
+                    } else if ((group == null) && (entry.Group != "")) {
+                        continue;  // if group is not set, skip entries with group
+                    }
+                    list.Add(entry);
+                }
             }
         }
 
